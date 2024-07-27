17aa4caf218fdfab0c27211a940d72242949021a
Tiger Tang <tigerhix@gmail.com>
2024-07-27 15:36:12 -0600

    Update server url

--- src/data/store.ts
+++ src/data/store.ts
@@ -4,7 +4,8 @@ import {SpectacleClient} from "../client/SpectacleClient.ts";
 import {InteractionType} from "./models.ts";
 
 export const useStore = defineStore('main', () => {
-  const serverUrl = ref(`ws://${window.location.hostname}:19876`)
+  // const serverUrl = ref(`ws://${window.location.hostname}:19876`)
+  const serverUrl = ref(`wss://owo.vtoku.com`)
   const client = ref(new SpectacleClient(serverUrl.value).connect())
   const interaction = ref({ type: InteractionType.Empty, id: '' })
   const countdownValue = ref(0)
